0b9b57f7749a9b37b9705c08042468961501d24a
ansh3108 <anshkumar9686@gmail.com>
2026-05-27 15:31:44 +0530

    db

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -4,13 +4,22 @@ import { prismaClient } from "db/client";
 
 const app = express();
 
-app.post("/api/v1/website", authMiddleware, (req, res) => {
-    const userId = req.userId;
+app.use(express.json());
 
-    prismaClient.user.create({
+app.post("/api/v1/website", authMiddleware, async (req, res) => {
+    const userId = req.userId!;
+    const { url } = req.body;
 
+    const data = await prismaClient.website.create({
+        data: {
+            userId,
+            url
+        }
     })
 
+    res.json({
+        id: data.id
+    })
 })
 
 app.get("/api/v1/website/status", authMiddleware, (req, res) => {
