ff9928a04253083b1cc0b93b587f0d4478483eb1
ansh3108 <anshkumar9686@gmail.com>
2026-05-27 15:17:36 +0530

    db

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -1,1 +1,29 @@
-console.log("Hello via Bun!");
+import express from "express"
+import { authMiddleware } from "./middleware";
+import { prismaClient } from "db/client";
+
+const app = express();
+
+app.post("/api/v1/website", authMiddleware, (req, res) => {
+    const userId = req.userId;
+
+    prismaClient.user.create({
+
+    })
+
+})
+
+app.get("/api/v1/website/status", authMiddleware, (req, res) => {
+
+})
+
+app.get("/api/v1/websites", authMiddleware, (req, res) => {
+
+})
+
+app.delete("/api/v1/website/", authMiddleware, (req, res) => {
+
+})
+
+
+app.listen(3000);
